1a94f95006db7a1d04c4fa8078f34bbdfe973341
william adams <wadams@athenasciences.com>
2017-11-20 01:43:58 -0500

    added current list to create new list page

--- app/new-list/new-list.js
+++ app/new-list/new-list.js
@@ -6,7 +6,11 @@ angular.module('myApp').controller('NewListCtrl', ['$scope', 'searchService',
     $scope.searchService = searchService;
     $scope.searchText = '';
     $scope.movie = {};
+    $scope.list = {};
     $scope.movieFound = false;
+    $scope.currentList = [];
+    $scope.successMessage = false;
+    $scope.invalidName = false;
 
     $scope.searchImdb = function(title) {
       $scope.searchService.searchImdb(title)
@@ -15,4 +19,23 @@ angular.module('myApp').controller('NewListCtrl', ['$scope', 'searchService',
         })
     }
 
+    $scope.addMovie = function(movie) {
+      if($scope.currentList.indexOf(movie) < 0) {
+        $scope.currentList.push(movie);
+      }
+      $scope.successMessage = true;
+      console.log('curentList: ', $scope.currentList);
+    }
+
+    $scope.createList = function() {
+      $scpe.invalidName = false;
+      if($scope.list.name === undefined || $scope.list.name.length < 1) {
+        $scope.invalidName = true;
+      } else {
+        $scope.listService.addList()
+          .then(function(res) {
+            console.log('res: ', res);
+          })
+      }
+    }
   }]);
